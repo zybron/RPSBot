963af6f49f0d0c176f995156a9b593d8f6076bce
zybron <zybron@gmail.com>
2019-02-25 17:18:28 -0500

    add rps static command, allow dm response

--- main.js
+++ main.js
@@ -18,8 +18,39 @@ var moji = ['\:scissors:', '\:full_moon_with_face:', '\:newspaper:'];
 // Create an event listener for messages
 
 client.on('message', message => {
-  if (message.channel.type === "dm") return; 
+  // if (message.channel.type === "dm") return; 
   if (!message.content.startsWith(config.prefix)) return; // Ignore messages that don't start with the prefix
+  if (message.content === '!rps static') {
+    var first_throw = opt[Math.floor(Math.random()*opt.length)];
+    var first_moji = moji[opt.indexOf(first_throw)];
+    var second_throw = opt[Math.floor(Math.random()*opt.length)];
+    var second_moji = moji[opt.indexOf[second_throw]];
+
+    message.reply('you threw ' + first_throw + '! ' + first_moji);
+    message.reply('I threw ' + second_throw + '! ' + second_moji);
+    if (opt.indexOf(first_throw) === opt.indexOf(second_throw)) {
+      // tie
+      message.reply('we tied!');
+    } else if (first_throw === 'scissors') {
+      if (second_throw === 'rock') {
+        message.reply('I won!');
+      } else {
+        message.reply('you won!');
+      }
+    } else if (first_throw === 'rock') {
+      if (second_throw === 'paper') {
+        message.reply('I won!');
+      } else {
+        message.reply('you won!');
+      }
+    } else if (first_throw === 'paper') {
+      if (second_throw === 'scissors') {
+        message.reply('I won!');
+      } else {
+        message.reply('you won!');
+      }
+    }
+  }
   if (message.content === '!rps') {
     var resp = opt[Math.floor(Math.random()*opt.length)];
     var gene = moji[opt.indexOf(resp)];
