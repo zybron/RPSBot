726baf932b5f8d11292470c50a72fe9539be2ba0
zybron <zybron@gmail.com>
2019-03-22 21:17:05 -0400

    logging and error handling

--- main.js
+++ main.js
@@ -20,49 +20,60 @@ var moji = ['\:scissors:', '\:full_moon_with_face:', '\:newspaper:'];
 client.on('message', message => {
   // if (message.channel.type === "dm") return; 
   if (!message.content.startsWith(config.prefix)) return; // Ignore messages that don't start with the prefix
-  if (message.content === config.prefix + 'help') {
+  if (message.content.toLocaleLowerCase() === config.prefix + 'help') {
     message.author.send('Use `' + config.prefix + 'rps` to throw a challenge. \n' + 
       'Use `' + config.prefix + 'rps static` to throw a challenge against me and I will show you the results. \n' + 
-      'Finally, `' + config.prefix + 'help` will show you this message.');
+      'Finally, `' + config.prefix + 'help` will show you this message.')
+      .then(value => console.log('sent help to ' + message.author))
+      .catch(error => console.log(error));
   }
-  if (message.content === config.prefix + 'rps static') {
+  if (message.content.toLocaleLowerCase() === config.prefix + 'rps static') {
     var first_throw = opt[Math.floor(Math.random()*opt.length)];
     var first_moji = moji[opt.indexOf(first_throw)];
     var second_throw = opt[Math.floor(Math.random()*opt.length)];
     var second_moji = moji[opt.indexOf(second_throw)];
 
-    message.reply('you threw ' + first_throw + '! ' + first_moji);
-    message.reply('I threw ' + second_throw + '! ' + second_moji);
+    sendMessage(message, 'you threw ' + first_throw + '! ' + first_moji);
+
+    sendMessage(message, 'I threw ' + second_throw + '! ' + second_moji);
+
     if (opt.indexOf(first_throw) === opt.indexOf(second_throw)) {
       // tie
-      message.reply('we tied!');
+      sendMessage(message, 'we tied!');
     } else if (first_throw === 'scissors') {
       if (second_throw === 'rock') {
-        message.reply('I won!');
+        sendMessage(message, 'I won!');
       } else {
-        message.reply('you won!');
+        sendMessage(message, 'you won!');
       }
     } else if (first_throw === 'rock') {
       if (second_throw === 'paper') {
-        message.reply('I won!');
+        sendMessage(message, 'I won!');
       } else {
-        message.reply('you won!');
+        sendMessage(message, 'you won!');
       }
     } else if (first_throw === 'paper') {
       if (second_throw === 'scissors') {
-        message.reply('I won!');
+        sendMessage(message, 'I won!');
       } else {
-        message.reply('you won!');
+        sendMessage(message, 'you won!');
       }
     }
   }
-  if (message.content === config.prefix + 'rps') {
+  if (message.content.toLocaleLowerCase() === config.prefix + 'rps') {
     var resp = opt[Math.floor(Math.random()*opt.length)];
     var gene = moji[opt.indexOf(resp)];
-    message.reply('you threw ' + resp + '! ' + gene);
+    sendMessage(message, 'you threw ' + resp + '! ' + gene);
   }
 });
+
 // Log our bot in
 client.login(config.token).catch((reason) => { 
   console.log(reason);
 });
+
+function sendMessage(message, text) {
+  message.reply(text)
+    .then(value => console.log('sent ' + text + ' to ' + message.author))
+    .catch(error => console.log(error));
+}
